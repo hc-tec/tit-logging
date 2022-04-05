c75ef057f9883c5473a8f3582614a2d206f9f6df
titto <2598772546@qq.com>
2022-04-05 15:38:42 +0800

    [add] test file

---
+++ main.cpp
@@ -0,0 +1,12 @@
+// Use of this source code is governed by a BSD-style license
+// that can be found in the License file.
+//
+// Author: Titto (2598772546@qq.com)
+
+
+#include <iostream>
+
+int main()
+{
+  std::cout << "Hello World" << std::endl;
+}
